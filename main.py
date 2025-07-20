aa1aa8c25723b0b4248f3840cc6fa14b42a87efc
tofipie <62646269+tofipie@users.noreply.github.com>
2025-07-20 09:22:44 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
 
 import streamlit as st
 
-st.markdown(
-"""
-
-.css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-.styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-.viewerBadge_text__1JaDK {
-display: none;
+hide_github_icon ="""
+#GithubIcon {
+visibility: hidden;
 }
-
-""",
-unsafe_allow_html=True
-)
+"""
+st.markdown(hide_github_icon, unsafe_allow_html=True)
 
 import random
 import time
